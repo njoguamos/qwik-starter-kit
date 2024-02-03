ed17990b048f556e2f4b20f9a71705500defbb1a
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-02-04 00:41:16 +0300

    Add re-usable actions

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,6 +1,11 @@
 import { Slot, component$ } from '@builder.io/qwik'
 import { routeLoader$ } from '@builder.io/qwik-city'
 
+//
+// import * from "~/utils/server"
+// import * from "~/utils/actions"
+// import * from "~/utils/loaders"
+
 export const useServerTimeLoader = routeLoader$(() => {
     return {
         date: new Date().toISOString(),
